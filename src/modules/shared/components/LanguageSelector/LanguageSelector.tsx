ffba2b505a46e5a88f7769f924e58edce8ec2fb6
Ferran <ferran.puigmar@gmail.com>
2022-01-25 09:36:25 +0100

    Chnage url when swith language

--- src/modules/shared/components/LanguageSelector/LanguageSelector.tsx
+++ src/modules/shared/components/LanguageSelector/LanguageSelector.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 import styles from './LanguageSelector.module.scss';
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
+  const location = useLocation();
   const currentLanguage = i18n.language;
 
   const changeLanguage = (lng: string) => {
+    const url = location.pathname;
+    const splitUrl = url.split('/');
+    splitUrl[1] = lng;
+    window.history.replaceState({}, '', splitUrl.join('/'));
     i18n.changeLanguage(lng);
   };
 
